41fdd4fe83a3bf7d70a196bb211a780ff5eb47a4
Lau Jing Lum <laujinglum@gmail.com>
2024-08-15 20:58:08 +0800

    Day 2 Exercise 1

---
+++ 02_Day_Variables_builtin_functions/variable_lum.py
@@ -0,0 +1,13 @@
+# Exercise 1
+# Day 2:30 Days of python programming
+first_name = 'Mickey'
+last_name = 'Gregory'
+full_name = 'Mickey Gregory'
+country = 'France'
+city = 'Paris baguette'
+age = 3
+year = 2021
+is_married = False
+is_true = True
+is_light_on = True
+last_name, age, country, city, is_married = 'Gregory', 150, 'Malaysia', 'KL', True
