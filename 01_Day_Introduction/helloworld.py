4f53c848ada592bba264859150e286888ef43d39
Lau Jing Lum <laujinglum@gmail.com>
2024-08-15 20:57:43 +0800

    Day 1 changes

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -11,11 +11,11 @@ print(3 // 2)  # Floor division operator(//)
 
 # Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
+print(type(10))                  # Integer (whole number)
+print(type(3.14))                # Float (decimal)
 print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type('Asabeneh'))          # String (words/numbers INSIDE 2 COMMAS '')
+print(type([1, 2, 3]))           # List (anything inside square brackets)
+print(type({'name':'Asabeneh'})) # Dictionary () dictionary['name']
+print(type({9.8, 3.14, 2.7}))    # Set (x duplicate values)
+print(type((9.8, 3.14, 2.7)))    # Tuple (x change)
